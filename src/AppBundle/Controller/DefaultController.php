9f79c30007c99a6833664b65b2f0b665bbd41469
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2019-01-15 19:14:54 +0100

    test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage")asdasdasd
      */
     public function indexAction(Request $request)
     {
